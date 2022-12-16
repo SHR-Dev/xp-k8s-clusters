023deff7b0671788ca9f109b2c530fe7f012a952
Matthew Ruge <matthew.ruge@shrgroupllc.com>
2022-12-16 16:37:02 -0500

    crossplane configure workflow works

--- mgmt-cluster/StartRUp.sh
+++ mgmt-cluster/StartRUp.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 kind delete cluster
-kind create cluster #--config=kind.yml
+kind create cluster --config=kind.yml
 
-helm install --wait --namespace management --create-namespace argocd argo-cd --repo=https://argoproj.github.io/argo-helm
+helm upgrade --install --wait --namespace management --create-namespace argocd argo-cd --repo=https://argoproj.github.io/argo-helm --set configs.secret.argocdServerAdminPassword='$2y$10$ZgBRKmHYy4tPe5.iEHvX2u.oEZ4.Tnc4LfQPT25IU02Cstma8zcEy'
 kubectl -n management apply -f mgmt-cluster/templates/vault_init.sh
 
 kubectl -n management apply -f Argo-Apps/
+
+# helm uninstall --namespace management argocd
