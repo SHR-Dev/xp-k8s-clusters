260fdbc61d00df1b5a20f3b0521fc16b43f469a4
Matthew Ruge <matthew.ruge@shrgroupllc.com>
2022-12-20 15:24:48 -0500

    ]

--- mgmt-cluster/StartRUp.sh
+++ mgmt-cluster/StartRUp.sh
@@ -2,20 +2,34 @@
 kind delete cluster
 kind create cluster --config=kind.yml
 
-helm upgrade --install --wait --namespace management --create-namespace argocd argo-cd --repo=https://argoproj.github.io/argo-helm --set configs.secret.argocdServerAdminPassword='$2y$10$ZgBRKmHYy4tPe5.iEHvX2u.oEZ4.Tnc4LfQPT25IU02Cstma8zcEy'
-kubectl -n management apply -f mgmt-cluster/templates/vault_init.sh
+k="kubectl -n management "
 
-kubectl -n management apply -f Argo-Apps/
+helm upgrade --install --wait \
+    --namespace management --create-namespace \
+    argocd argo-cd \
+    --repo=https://argoproj.github.io/argo-helm \
+    --set configs.secret.argocdServerAdminPassword='$2y$10$ZgBRKmHYy4tPe5.iEHvX2u.oEZ4.Tnc4LfQPT25IU02Cstma8zcEy'
 
-kubectl -n management wait --for=jsonpath='{.status.health.status}'=Healthy Application -l management-app=Workflows --timeout=5m
-kubectl -n management wait --for condition=established --timeout=5m crd/workflows.argoproj.io
+${k} apply -f mgmt-cluster/templates/vault_init.sh
+
+${k} apply -f Argo-Apps/
+
+${k} wait --timeout=5m \
+    --for=jsonpath='{.status.health.status}'=Healthy \
+    Application -l management-app=Workflows 
+${k} wait --timeout=5m\
+    --for condition=established  \
+    crd/workflows.argoproj.io
 
 
 
-kubectl -n management apply -f mgmt-cluster/templates/configure-workflow.yaml
+${k} apply \
+    -f mgmt-cluster/templates/configure-workflow.yaml
 
-kubectl -n management wait --for=jsonpath='{.status.phase}'=Completed Workflow -l configures=crossplane  --timeout=10m
+${k} wait --timeout=20m \
+    --for=jsonpath='{.status.phase}'=Succeeded \
+    Workflow -l configures=crossplane  
 
 
-
+${k} apply -f ../proj_comp/provider-config.yml.bak
 # helm uninstall --namespace management argocd
